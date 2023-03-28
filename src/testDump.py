2e11145ff9e570a3501112b95ec5e7aed3cde149
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2023-03-29 01:13:30 +0530

    Created, added material from test.py

---
+++ src/testDump.py
@@ -0,0 +1,21 @@
+
+# ----> Miscellaneous <----
+test = [
+    {
+        "1" : {"Quote": "AA", "Fact":"True"},
+        "2" : {"Quote": "BB", "Fact":"False"},
+        "3" : {"Quote": "CC", "Fact":"True"}
+    }
+]
+
+x = "1"
+
+print(test[0]["1"]["Fact"])
+
+# ----> From input.py <----
+
+sentiment_pipeline = pipeline("sentiment-analysis")
+specific_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+data = ["I love you", "I hate you"]
+print(sentiment_pipeline(data))
+print(specific_model(data))
