04208add42cc5742fd7543a1ae320943c464de58
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2023-04-11 23:53:34 +0530

    </> Commented illogical code

--- src/testDump.py
+++ src/testDump.py
@@ -14,8 +14,8 @@ print(test[0]["1"]["Fact"])
 
 # ----> From input.py <----
 
-sentiment_pipeline = pipeline("sentiment-analysis")
-specific_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
-data = ["I love you", "I hate you"]
-print(sentiment_pipeline(data))
-print(specific_model(data))
+# sentiment_pipeline = pipeline("sentiment-analysis")
+# specific_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+# data = ["I love you", "I hate you"]
+# print(sentiment_pipeline(data))
+# print(specific_model(data))
