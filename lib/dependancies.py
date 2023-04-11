dc9cc320bb80c5e7d0a97cc76bd93cd58dc3197f
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2023-04-11 23:53:05 +0530

    <+> Added more dependencies

--- lib/dependancies.py
+++ lib/dependancies.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 from transformers import pipeline, BlenderbotTokenizer, TFBlenderbotForConditionalGeneration, AutoModel
 from wordcloud import WordCloud
 from wordcloud import STOPWORDS
