453a68202cce5928a1f593907526f6c0c184ba2e
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2023-04-28 14:24:23 +0530

    add functionality

--- scripts/app.py
+++ scripts/app.py
@@ -1,43 +1,43 @@
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import cross_origin, CORS
+import connectAPI as CAP
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = "Content-Type"
+CORS(app)
 
-@app.route('/receive_message', methods=['POST', 'GET'])
+# cors = CORS(app, resources={r"/receive_message": {"origins": "http://localhost:5500"}})
+
+@app.route('/receive_message', methods=['POST', 'GET', 'OPTIONS'])
+# @cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def receive_message():
-    data = request.get_json()
-    #resp = requests.get("http://127.0.0.1:8000/receive_message")
-    # data = resp.json()
-    message = data['message']
+    word = " "
+    if request.method == "GET":
+        return "Get Success" 
     return "Success"
+
+@app.route('/post_test', methods=['POST'])
+# @cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+def post_message():
+    if request.method == "POST":
+        fileAppend = open('context.txt', 'a')
+        fileRead = open('context.txt', 'r')
+        fileData = str(fileRead.readline())
+
+        message = request.get_json()
+        userInput = message["message"]
+        if userInput == "chatStop":
+            response = CAP.chat("chatStop")
+        else:
+            response = CAP.chat(fileData + "User:" + userInput)
+
+            fileAppend.write("User:" + userInput + "Bot:" +response)
+
+        fileAppend.close()
+        fileRead.close()
+
+        return response
 
 if __name__ == '__main__':
     app.run(host = "localhost", port=8000, debug = True)
-
-
-# @app.route('/http://localhost:5000/api/v1', methods=['GET', 'PUT', 'POST', 'DELETE'])
-# def chat():
-#     # Get user input from frontend
-#     user_input = request.json.get('input')
-
-#     # AI model prompt
-#     prompt = f"Bot: Hi, how can I assist you today?\nUser: {user_input}\nBot:"
-
-#     # AI model parameters
-#     data = {
-#         "prompt": prompt,
-#         "max_tokens": 50,
-#         "temperature": 0.7,
-#     }
-
-#     # Send request to OpenAI API to generate a response
-#     headers = {
-#         "Content-Type": "application/json",
-#         "Authorization": "Bearer " + "YOUR_API_KEY"
-#     }
-#     response = requests.post(url_main+url_gen, data=json.dumps(data), headers=headers)
-#     response_json = response.json()
-#     chat_response = response_json["choices"][0]["text"].strip()
-
-#     # Return response to frontend
-#     return jsonify({'response': chat_response})
